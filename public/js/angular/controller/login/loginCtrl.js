65fd6b8b56721dddea97153a92bf04dd37608a4b
diogom10 <diogomoura10@gmail.com>
2017-11-07 19:56:33 -0200

    popover on

--- public/js/angular/controller/login/loginCtrl.js
+++ public/js/angular/controller/login/loginCtrl.js
@@ -3,14 +3,21 @@ angular.module("sigere_login", []);
 angular.module("sigere_login").controller("loginCtrl", ['$scope', '$http', 'loginService', function ($scope, $http, loginService) {
 
         $scope.teste = function () {
-            alert('paulao');
+
+
+
+
+
         };
-        
-//            var popoverDynamic = angular.element("#view");
-//            $('.screen').show();
-//            $('[data-toggle="popover"]').popover({html: true, content: popoverDynamic, placement: 'right'});
-        
-          $(document).ready(function(){
-    $('[data-toggle="popover"]').popover(); 
-});
+
+        $(document).ready(function () {
+            var PopoverTags = angular.element("#view");
+            angular.element('body').on('mouseenter', '[data-toggle="popover"]', function () {
+                var Este = $(this);
+                if (!Este.is('.has-popover')) {
+                    Este.popover({content: PopoverTags, html: true, placement: 'bottom', container: 'body'});
+                    Este.addClass('has-popover');
+                }
+            });
+        });
     }]);
--- resources/views/view_login.php
+++ resources/views/view_login.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-BR" ng-app="sigere_login">
     <head>
-         <link type="text/css" rel="stylesheet" href="<?= url('/');?> /sass/bootstrap.min.css">
-         <script src="<?= url('/'); ?>/js/jquery-3.2.1.js"></script>
-         <script src="<?= url('/'); ?>/js/angular.min.js"></script>
-         <script src="<?= url('/'); ?>/js/bootstrap.js"></script>
-         <script src="<?= url('/'); ?>/js/angular/controller/login/loginCtrl.js"></script>
-         <script src="<?= url('/'); ?>/js/angular/service/login/loginService.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="<?= url('/'); ?>/js/angular.min.js"></script>
+        <script src="<?= url('/'); ?>/js/angular/controller/login/loginCtrl.js"></script>
+        <script src="<?= url('/'); ?>/js/angular/service/login/loginService.js"></script>
     </head>
     <body>
 
         <div ng-controller="loginCtrl">
-        <h1 ng-click="teste()">PRIMEIRA VIEW</h1>
+            <h1 >PRIMEIRA VIEW</h1>
+            <div class="container-fluid">
+                <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+                    <button  type="button" ng-click="teste()" class="btn btn-danger" data-toggle="popover">teste</button>
+                    <?= view('view_login_cadastro')->render() ?>
+                </div>
+            </div>
 
-        
-        <div class="col-md-4 col-sm-4 col-xs-4 col-lg-4"></div>
-        <button class="btn btn-danger" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">teste</button>
-        <div>teste<div>
-         <?= view('view_login_cadastro')->render() ?>
-        </div>
-        
-      
-        
+
+
     </body>
 </html>
